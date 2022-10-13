342ebe0002aa0e9023bd1e56de6d27ab0c5e0426
hoangdev01 <dbvhoang@gmail.com>
2022-10-13 17:05:29 +0700

    feat: login-logout by api
    Replace all file and folder

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+cd backend<br>
+python -m venv ./env<br>
+pip install -r .\authtutorial\requirements.txt<br>
+python manage.py runserver
--- backend/main/helpers.py
+++
@@ -1,7 +0,0 @@
-def handle_uploaded_file(f):  
-    app = "main/"
-    basePath = "static/upload/"
-    with open(app + basePath + f.name, 'wb+') as destination:  
-        for chunk in f.chunks():  
-            destination.write(chunk) 
-    return basePath + f.name
