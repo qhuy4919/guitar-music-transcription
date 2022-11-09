9487f9f1b5b1f1b9146e283a88e6159088dce132
hoangdev01 <dbvhoang@gmail.com>
2022-11-09 20:22:45 +0700

    fix: type name none error

--- backend/audio/views.py
+++ backend/audio/views.py
@@ -66,7 +66,7 @@ class uploadAPIView(APIView):
             'success': True,
             'audio': tablature.audio.name,
             'tablature': tablature.code,
-            'type': tablature.type.name
+            'type': tablature.type.name if tablature.type.name else None
         })
 
 class listAudioAPIView(APIView):
