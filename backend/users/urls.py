4c1a96b834bc7d71eed6c88d5c49242a6b7b9caf
hoangdev01 <dbvhoang@gmail.com>
2022-12-25 17:41:30 +0700

    feat: profile and change password

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('register/', registerAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
     path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    path('profile/', profile.as_view()),
+    path('change-password/', changePassword.as_view())
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserSerializer
+from .serializers import *
 from .models import User
 from rest_framework.exceptions import AuthenticationFailed
 
 import jwt
 import datetime
+import json
 
 # Create your views here.
+
+def getCurrentUserId(request):
+    
+    # token = request.COOKIES.get('jwt')
+    token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
+
+    if not token:
+        raise "User is not authenticated."
+    
+    try:
+        payload = jwt.decode(token, 'secret', algorithms="HS256")
+
+    except jwt.ExpiredSignatureError:
+        raise "User is not authenticated."
+    
+    user = User.objects.filter(id=payload['id']).first()
+    serializer = UserSerializer(user)
+
+    return serializer.data["id"]
+
 
 class registerAPIView(APIView):
     def post(self, request):
@@ -88,3 +109,82 @@ class LogoutView(APIView):
         }
 
         return response
+
+class profile(APIView):
+    def get(self, request):
+        try:
+            user_id = getCurrentUserId(request)
+            user = User.objects.get(id=int(user_id))
+        except:
+            return Response({
+                'success': False,
+                'message': 'User is not authenticated.'
+            })
+        data = {
+            "name": user.name if user.name else "",
+            "username": user.username if user.username else "",
+            "email": user.email if user.email else ""
+        }
+        return Response(data)
+
+    def post(self, request):
+        if 'name' in request.POST or "email" in request.POST:
+            if 'name' in request.POST and request.POST["name"] == "":
+                return Response({"success": False, "message": "Name can not blank"})
+            if "email" in request.POST and request.POST["email"] == "":
+                return Response({"success": False, "message": "Email can not blank"})
+        else:
+            return Response({"success": False, "message": "Please fill in the information that needs to be changed"})
+        try:
+            user_id = getCurrentUserId(request)
+            user = User.objects.get(id=int(user_id))
+            if "name" in request.POST:
+                user.name = request.POST["name"] if request.POST["name"] != user.name else user.name
+            if "email" in request.POST:
+                user.email = request.POST["email"] if request.POST["email"] != user.email else user.email
+        except:
+            return Response({
+                'success': False,
+                'message': 'User is not authenticated.'
+            })
+        user_mail_check = User.objects.filter(email=request.POST["email"]).first()
+        if user_mail_check and user_mail_check.id != user.id and user_mail_check.email == request.POST["email"]:
+            return Response({
+                'success': False,
+                'message': 'Email already exists'
+            })
+        user.save()
+        return Response({
+            'success': True,
+            'message': 'Change information successful'
+        })
+
+class changePassword(APIView):
+    def post(self, request):
+        try:
+            try:
+                user_id = getCurrentUserId(request)
+                user = User.objects.get(id=int(user_id))
+                if "old_password" not in request.POST or not user.check_password(request.POST["old_password"]):
+                    raise AuthenticationFailed('Invalid password')
+            except:
+                return Response({
+                    'success': False,
+                    'message': 'User is not authenticated.'
+                })
+            if "new_password" not in request.POST or len(request.POST["new_password"]) < 6:
+                return Response({
+                    'success': False,
+                    'message': 'Password length must > 6.'
+                })
+            user.set_password(request.POST["new_password"])
+            user.save()
+            return Response({
+                "success": True,
+                "message": "Change password successful"
+            })
+        except:
+            return Response({
+                "success": False,
+                "message": "Internal server error"
+            })
